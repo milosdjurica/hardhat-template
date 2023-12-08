2789250b2d9eb68d2b811fe796a458b80abf5223
milosdjurica <milosdjurica13@gmail.com>
2023-12-08 17:57:39 +0100

    added conventions.md

---
+++ contracts/Name.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+// TODO -> add comments for conventions -> imports, errors, variables, etc....
+
+contract Name {
+    constructor() {}
+}
