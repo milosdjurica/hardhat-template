5c55996c33b3366e81aa656a023e93ebbc9af4d2
milosdjurica <milosdjurica13@gmail.com>
2023-12-08 17:34:21 +0100

    added hardhat-deploy package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 
+import "hardhat-deploy";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
 };
