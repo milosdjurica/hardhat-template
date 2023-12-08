d33a714fd9b4ea0c18248fe9880727991898eafc
milosdjurica <milosdjurica13@gmail.com>
2023-12-08 17:47:52 +0100

    hardhat.config.ts basic setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,49 @@ import "dotenv/config";
 
 import "hardhat-deploy";
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_PROCESS_ENV || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xKEY";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // blockConfirmations: 3
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      // blockConfirmations: 1,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    // put it enabled: true -> only when you want to check gas optimizations
+    enabled: false,
+    noColors: true,
+    outputFile: "gas-report.txt",
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "MATIC", // polygon network
+  },
+  mocha: {
+    timeout: 500000, //500 seconds
+  },
 };
 
 export default config;
