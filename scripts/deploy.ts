c048548a86fcc522c231a0afb461a501c3dc39cf
milosdjurica <milosdjurica13@gmail.com>
2023-12-08 17:37:09 +0100

    setup deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,18 @@
-import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  const contractName = await deploy("ContractName", {
+    from: deployer,
+    args: [], // ! constructor args
+    log: true,
   });
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.log(`ContractName contract: `, contractName.address);
+};
+export default func;
+func.id = "deploy_contractName"; // id required to prevent reexecution
+func.tags = ["ContractName"];
