02ae4d7b9a75b8b3c88f904ad981be7ffceed0f2
milosdjurica <milosdjurica13@gmail.com>
2023-12-09 18:28:22 +0100

    added conventions in contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,7 +2,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-	const { deployer } = await hre.getNamedAccounts();
+	const { deployer, log } = await hre.getNamedAccounts();
 	const { deploy } = hre.deployments;
 
 	const contractName = await deploy("ContractName", {
