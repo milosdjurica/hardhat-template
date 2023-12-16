6b0ab72505e2be8bdf6f9f6c0abca619d9f805f1
milosdjurica <milosdjurica13@gmail.com>
2023-12-16 19:06:39 +0100

    added developmentChains

--- test/unit/Example.unit.test.ts
+++ test/unit/Example.unit.test.ts
@@ -1,7 +1,17 @@
-describe("Example Unit Tests", () => {
-	beforeEach(async () => {
-		describe("Constructor Tests", () => {
-			it("Example test", async () => {});
-		});
-	});
-});
+import { network, ethers, getNamedAccounts, deployments } from "hardhat";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { assert, expect } from "chai";
+
+import { developmentChains } from "../../utils/helper.config";
+
+const isDevelopmentChain = developmentChains.includes(network.name);
+
+!isDevelopmentChain
+	? describe.skip
+	: describe("Example Unit Tests", () => {
+			beforeEach(async () => {
+				describe("Constructor Tests", () => {
+					it("Example test", async () => {});
+				});
+			});
+	  });
