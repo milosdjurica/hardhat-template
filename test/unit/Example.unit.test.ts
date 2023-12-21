df6727b4c970e1c6e32dee5187bccc42b69ad37c
milosdjurica <milosdjurica13@gmail.com>
2023-12-21 15:06:19 +0100

    fixed tests and updated hardhat version to latest

--- test/unit/Example.unit.test.ts
+++ test/unit/Example.unit.test.ts
@@ -5,13 +5,18 @@ import { assert, expect } from "chai";
 import { developmentChains } from "../../utils/helper.config";
 
 const isDevelopmentChain = developmentChains.includes(network.name);
+console.log("unit test");
 
 !isDevelopmentChain
 	? describe.skip
 	: describe("Example Unit Tests", () => {
 			beforeEach(async () => {
-				describe("Constructor Tests", () => {
-					it("Example test", async () => {});
+				// ! Do some code here
+			});
+
+			describe("Constructor Tests", () => {
+				it("Example test", async () => {
+					assert.equal(1, 1);
 				});
 			});
 	  });
